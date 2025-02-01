16b9c58c336f67cb2d2d0b99b485cf2e72b5bb69
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-31 21:19:36 -0500

    Update integration-test-all.sh

--- scripts/integration-test-all.sh
+++ scripts/integration-test-all.sh
@@ -11,7 +11,7 @@ TEST_QTD=1
 
 #PORT AND RPC_PORT 3 initial digits, to be concat with a suffix later when node is initialized
 RPC_PORT="854"
-IP_ADDR="0.0.0.0"
+IP_ADDR="75.135.130.6"
 MODE="rpc"
 
 KEY="mykey"
