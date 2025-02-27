2aa18e86500e32d629d9370d09c5a7b491ec29f3
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-02-27 16:08:54 -0500

    Update ethermint_test.go

--- app/ethermint_test.go
+++ app/ethermint_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/VersoriumX/testify/require"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
