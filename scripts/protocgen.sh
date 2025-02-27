00f88495895538e555305547c4268dea1913bb59
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-02-27 14:46:38 -0500

    Update protocgen.sh

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,10 +2,10 @@
 
 set -eo pipefail
 
-proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . -path ./third_party -prune -o -name '*' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   protoc \
   -I. \
   --gocosmos_out=plugins=interfacetype,paths=source_relative:. \
-  $(find "${dir}" -name '*.proto')
+  $(find "${dir}" -name '*')
 done
